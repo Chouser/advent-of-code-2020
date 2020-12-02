4eb8a791e04037df4e5ae18a5b6a1704d31530e8
Chouser <chouser@n01se.net>
2020-12-02 10:39:58 -0500

    Add some comments for day 1

--- src/chouser/day01.clj
+++ src/chouser/day01.clj
@@ -3,6 +3,14 @@
 
 ;; Find a pair of numbers from the set whose sum is 2020
 
+;; I've done a couple Advents before, so I can predict I'll need a way to read
+;; stuff from files. I've been occasionally irritated by the lack of a
+;; convenient way to read a file full of edn expressions, so I thought I'd try
+;; to make something that is abstract enough for reuse, eager (to avoid problems
+;; with lazy seqs of files that need to be closed), but that doesn't realize the
+;; entire file of forms before processing begins. This clearly needs refinement,
+;; and I assume others have written something equivalent or better, but this is
+;; what I've got so far:
 (defn read-all [filename]
   (reify p/CollReduce
     (coll-reduce [eof f]
@@ -17,6 +25,8 @@
                 start
                 (repeatedly #(read {:eof eof} reader)))))))
 
+;; Pouring input-seq into a set should be O(n * log32(n)), as is walking the set
+;; doing a lookup for each entries partner.
 (defn multiply-pair [sum-goal input-seq]
   (let [input-set (into (hash-set) input-seq)]
     (some (fn [i]
@@ -34,6 +44,8 @@
 
 ;; Find three numbers from the set whose sum is 2020
 
+;; Can't think of how to do this without essentially summing each possible pair
+;; before looking up the third entry. So that O(n^2 * log32(n)). Ouch.
 (defn multiply-triple [sum-goal input-seq]
   (some (fn [i]
           (when-let [product (multiply-pair (- sum-goal i) input-seq)]
